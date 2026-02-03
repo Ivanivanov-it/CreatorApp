d1ad8bef31e697b820de40c85334e4f29acd7cbd
ChunChun20 <113472390+ChunChun20@users.noreply.github.com>
2026-02-03 11:37:11 +0200

    added characters html pages
    Filled the database with custom data

--- characters/views.py
+++ characters/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from characters.models import Character
+
 
 # Create your views here.
 
@@ -13,10 +16,23 @@ def landing_page(request: HttpRequest) -> HttpResponse:
     return render(request,'characters/landing_page.html', context)
 
 def characters_list(request: HttpRequest) -> HttpResponse:
-    pass
+    characters = Character.objects.all()
+
+    context = {
+        'page_title': "Characters",
+        'characters': characters,
+    }
+
+    return render(request,'characters/characters_page.html',context)
 
 def character_detail(request: HttpRequest,id: int) -> HttpResponse:
-    pass
+    character = get_object_or_404(Character, pk=id)
+    context = {
+        'page_title': f"{character.name} Details",
+        'character': character,
+    }
+
+    return render(request,'characters/character_page.html',context)
 
 def create_character(request: HttpRequest) -> HttpResponse:
     pass
@@ -25,4 +41,9 @@ def edit_character(request: HttpRequest,id: int) -> HttpResponse:
     pass
 
 def delete_character(request: HttpRequest,id: int) -> HttpResponse:
-    pass
+    character = get_object_or_404(Character, pk=id)
+
+    if character:
+        character.delete()
+
+    return redirect('characters:characters_list')
