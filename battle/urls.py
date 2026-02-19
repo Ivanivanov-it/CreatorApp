977e6c9d0097e934c4c16013022f6944ae56cf16
Ivanivanov-it <113472390+Ivanivanov-it@users.noreply.github.com>
2026-02-19 06:30:57 +0200

    created select partner and select enemy templates

--- battle/urls.py
+++ battle/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from battle.views import character_selection, enemy_selection, battle
+from battle.views import character_selection, enemy_selection, battle, partner_selection
 
 app_name = 'battle'
 
 
 urlpatterns = [
     path('character-selection/',character_selection, name='character_selection'),
+    path('partner-selection/',partner_selection, name='partner_selection'),
     path('enemy-selection/',enemy_selection,name="enemy_selection"),
     path('battle/',battle, name='battle'),
 ]
--- battle/views.py
+++ battle/views.py
@@ -3,6 +3,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
 from characters.models import Character
+from enemies.models import Enemy
+from partners.models import Partner
 
 
 # Create your views here.
@@ -15,7 +17,9 @@ def character_selection(request: HttpRequest) -> HttpResponse:
 
         request.session["character_id"] = character_id
 
-        return redirect("battle:enemy_selection")
+        print(character_id)
+
+        return redirect("battle:partner_selection")
 
     characters = Character.objects.all()
 
@@ -26,9 +30,57 @@ def character_selection(request: HttpRequest) -> HttpResponse:
 
     return render(request,"battle/select_character.html",context=context)
 
+def partner_selection(request: HttpRequest) -> HttpResponse:
+
+    character_id = request.session.get("character_id")
+
+    if not character_id:
+        return redirect("battle:character_selection")
+
+    partners = Partner.objects.filter(character_id=character_id)
+
+    if request.method == "POST":
+        partner_id = request.POST.get("partner_id")
+
+
+        request.session["partner_id"] = partner_id
+
+        return redirect("battle:enemy_selection")
+
+
+    context = {
+        'partners': partners,
+         'page_title': "Partner Selection"
+    }
+
+    return render(request,"battle/select_partner.html",context=context)
+
 
 def enemy_selection(request: HttpRequest) -> HttpResponse:
-    pass
+
+    character_id = request.session.get("character_id")
+
+    if not character_id:
+        return redirect("battle:character_selection")
+
+
+
+    if request.method == "POST":
+        enemy_id = request.POST.get("enemy_id")
+
+
+        request.session["enemy_id"] = enemy_id
+
+        return redirect("battle:battle")
+
+    enemies = Enemy.objects.all()
+
+    context = {
+        'enemies': enemies,
+         'page_title': "Enemy Selection"
+    }
+
+    return render(request,"battle/select_enemy.html",context=context)
 
 def battle(request: HttpRequest) -> HttpResponse:
     pass
