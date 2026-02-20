b846e5c14aeb710b0e18a93bb0588794ccdf7669
Ivanivanov-it <113472390+Ivanivanov-it@users.noreply.github.com>
2026-02-20 02:44:03 +0200

    added custom filter to show characters hp % in battle

--- common/templatetags/custom_tags.py
+++ common/templatetags/custom_tags.py
@@ -10,3 +10,10 @@ def show_roles(obj):
 def show_stats(obj):
 
     return {'attack': obj.attack, 'defense': obj.defense, "hp": obj.hp}
+
+@register.filter
+def hp_percent(obj):
+
+    hp = round((obj.current_hp / obj.max_hp) * 100, 1)
+
+    return hp if hp > 0 else 0
