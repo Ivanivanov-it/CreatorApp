15960a3db66f3d756fa81ae01c19273bc9ecfbe9
Ivanivanov-it <113472390+Ivanivanov-it@users.noreply.github.com>
2026-02-20 02:28:58 +0200

    added custom tag to include stats

--- common/templatetags/custom_tags.py
+++ common/templatetags/custom_tags.py
@@ -6,3 +6,7 @@ register = template.Library()
 def show_roles(obj):
     return {'roles': obj.roles.all()}
 
+@register.inclusion_tag('show_stats.html')
+def show_stats(obj):
+
+    return {'attack': obj.attack, 'defense': obj.defense, "hp": obj.hp}
