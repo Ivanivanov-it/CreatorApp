ad4051a16f8319fbefd3a506e016e310225a861c
Ivanivanov-it <113472390+Ivanivanov-it@users.noreply.github.com>
2026-03-16 10:46:45 +0200

    UPDATE: Changed FBV to CBV in contacts app and updated urls

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from contacts.views import create_mail, WipPage, AboutPageView
+from contacts.views import  WipPage, AboutPageView, CreateMailView
 
 app_name = 'contacts'
 
 urlpatterns = [
-    path('',create_mail,name='contact-us'),
+    path('',CreateMailView.as_view(),name='contact-us'),
     path('wip/',WipPage.as_view(),name='wip'),
     path('about/',AboutPageView.as_view(),name='about')
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,26 +1,22 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
 
 from contacts.forms import ContactForm
 
 
 # Create your views here.
 
-def create_mail(request: HttpRequest) -> HttpResponse:
-    form = ContactForm(request.POST or None)
 
-    if request.method == "POST" and form.is_valid():
-        form.save()
 
-        return redirect('characters:home')
-
-    context = {
-        'page_title': "Contact Us",
-        'form': form,
+class CreateMailView(CreateView):
+    form_class = ContactForm
+    success_url = reverse_lazy('characters:home')
+    template_name = 'contacts/contact.html'
+    extra_context = {
+        'page_title': "Contact Us"
     }
 
-    return render(request,'contacts/contact.html',context)
 
 
 class WipPage(TemplateView):
