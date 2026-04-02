ef42654a571204106e3ee27509ca10d46ca45703
Ivanivanov-it <113472390+Ivanivanov-it@users.noreply.github.com>
2026-04-02 14:27:13 +0300

    Celery Update

--- CreatorApp/celery.py
+++ CreatorApp/celery.py
@@ -4,8 +4,6 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CreatorApp.settings.local')
 
 app = Celery('CreatorApp')
-app.conf.broker_use_ssl = {'ssl_cert_reqs': None}
-app.conf.redis_backend_use_ssl = {'ssl_cert_reqs': None}
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
