fa2f499c60c3ebe0f9b178bd25203315f9db3645
Ivanivanov-it <113472390+Ivanivanov-it@users.noreply.github.com>
2026-04-02 13:33:51 +0300

    Updated Celery

--- CreatorApp/celery.py
+++ CreatorApp/celery.py
@@ -3,7 +3,12 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CreatorApp.settings.local')
 
-app = Celery('CreatorApp')
+app = Celery('CreatorApp',
+             broker=os.getenv('CELERY_BROKER_URL'),
+             backend=os.getenv('CELERY_BROKER_URL'),
+                broker_use_ssl={'ssl_cert_reqs': None},
+                redis_backend_use_ssl={'ssl_cert_reqs': None}
+             )
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 celery -A CreatorApp worker --loglevel=info &
+
+gunicorn CreatorApp:app
