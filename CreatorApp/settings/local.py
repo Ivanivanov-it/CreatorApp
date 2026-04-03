0e5f97000e100d7774a57474c08107e585a99181
Ivanivanov-it <113472390+Ivanivanov-it@users.noreply.github.com>
2026-04-03 08:55:20 +0300

    Updated security settings

--- CreatorApp/settings/local.py
+++ CreatorApp/settings/local.py
@@ -6,3 +6,11 @@ ALLOWED_HOSTS = [host for host in (os.getenv('ALLOWED_HOSTS') or "").split(',') 
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
+
+SECURE_SSL_REDIRECT = False
+SECURE_HSTS_SECONDS = 0
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_PRELOAD = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
